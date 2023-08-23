1b6de83f3282a49c8782bae02d0f4b0d84dcb878
Uday <admin@udayteja.com>
2023-08-23 19:46:16 +0530

    Add Cart Create Route, Controller and Model

--- routes/cart.routes.js
+++ routes/cart.routes.js
@@ -1,7 +1,6 @@
 const cartController = require("../controllers/cart.controller");
 const { authUser } = require("../middlewares/index");
 
-
 module.exports = (app) => { 
     // create a new cart
     app.post("/app/api/v1/cart", [authUser.verifyToken], cartController.addToCart);
