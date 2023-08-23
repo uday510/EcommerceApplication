8d6c4388c3815dd020e50fada0baa46ae12223f9
Uday <admin@udayteja.com>
2023-08-23 10:46:53 +0530

    Add MongoDB Cloud Connection

---
+++ configs/db.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    DB_NAME: "ecommerceDB",
+    DB_URL:
+        "mongodb+srv://uday:pNeV1GtGYXrsj4WZ@cluster0.xv1rnyy.mongodb.net/",
+}
+
+// pNeV1GtGYXrsj4WZ --> ecommerceDB password
--- server.js
+++ server.js
@@ -1,8 +1,21 @@
 const express = require('express');
+const mongoose = require("mongoose");
+const dbConfig = require("./configs/db.config");
 
 const app = express();
 
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// DB connection
+mongoose
+    .connect(dbConfig.DB_URL, {
+        useNewUrlParser: true, // To avoid Deprecation Warning
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        app.listen(4000);
+        console.log("Database connected and server running at port 4000");
+    })
+    .catch((err) => {
+        // throw err;
+        console.log("Database connection error", err.message);
+    });
