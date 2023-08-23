e1fe9b88e271303ca8a747ba5c375f8412e70f23
Uday <admin@udayteja.com>
2023-08-23 12:01:11 +0530

    Add Category Controller and Error Handler for unknown API End Points

--- models/category.model.js
+++ models/category.model.js
@@ -8,4 +8,4 @@ const categorySchema = new mongoose.Schema({
     },
 });
 
-module.exports = mongoose.model('Category', categorySchema);x 
+module.exports = mongoose.model('Category', categorySchema);
--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const mongoose = require("mongoose");
 const dbConfig = require("./configs/db.config");
+const bodyParser = require("body-parser");
 
 const app = express();
 
+// Bodyparser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+require("./routes")(app);
+
+// Error handler
+app.use((req, res) => {
+    // center 404 error handler
+    res.status(404).send( {url: req.originalUrl + ' not found on this server'});
+});
 
 // DB connection
 mongoose
